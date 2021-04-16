af824d4a278df43dfd8c8d9f0516ff92a6bf1c84
aptamil-bc <48873250+aptamil-bc@users.noreply.github.com>
2021-04-16 21:54:36 +0800

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -4,7 +4,7 @@ export DB_ADDR="127.0.0.1:27017"
 export DB_DATABASE="shinecloudnet-explorer"
 export DB_USER="shinecloudnet"
 export DB_PASSWD="shinecloudnetpassword"
-export ADDR_NODE_SERVER="localhost:1317"
+export ADDR_NODE_SERVER="http://localhost:1317"
 export ADDR_HUB_RPC="http://3.112.87.138:26657"
 export CHAIN_ID="shinecloudnet"
 
